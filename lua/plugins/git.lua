fc0acbce544b419f2d79c1575a6d73e3206617c2
DJ <djp@headwire.com>
2024-11-23 09:21:18 -0700

    remove duplicated keymaps update git keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,30 +1,33 @@
 return {
-  {
-    'lewis6991/gitsigns.nvim', -- git status in gutters
-    config = function()
-      require('gitsigns').setup {
-        signs = {
-          add = { text = '+' },
-          change = { text = '~' },
-          delete = { text = '_' },
-          topdelete = { text = '‾' },
-          changedelete = { text = '~' },
-        },
-      }
-    end
-  },
+	{
+		"lewis6991/gitsigns.nvim", -- git status in gutters
+		config = function()
+			local gitsigns = require("gitsigns")
+			gitsigns.setup({
+				signs = {
+					add = { text = "+" },
+					change = { text = "~" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+				},
+			})
+			vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk)
+			vim.keymap.set("n", "<leader>bl", gitsigns.blame_line)
+		end,
+	},
 
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      "sindrets/diffview.nvim",        -- optional - Diff integration
-      "nvim-telescope/telescope.nvim", -- optional
-    },
-    config = true
-  },
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = true,
+	},
 
-  {
-     "sindrets/diffview.nvim"
-  }
+	{
+		"sindrets/diffview.nvim",
+	},
 }
