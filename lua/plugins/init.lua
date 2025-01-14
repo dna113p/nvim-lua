f5f1b4ed761e750aebb3839659c78298c4712409
DJ <djp@headwire.com>
2025-01-14 13:40:05 -0700

    remove some stuff

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,107 +1,85 @@
 return {
-  {
-    'Wansmer/treesj', -- Split and join blocks of code
-    dependencies = { 'nvim-treesitter' },
-    config = function()
-      require('treesj').setup()
-    end,
-  },
+	{
+		"Wansmer/treesj", -- Split and join blocks of code
+		dependencies = { "nvim-treesitter" },
+		config = function()
+			require("treesj").setup()
+		end,
+	},
 
-  { 'tpope/vim-sleuth' }, -- automatically tries to adjust spacing based on file heuristics
+	{ "tpope/vim-sleuth" }, -- automatically tries to adjust spacing based on file heuristics
 
-  { 'chaoren/vim-wordmotion' }, -- changes word jumps for CamelCase and underscore
+	{ "chaoren/vim-wordmotion" }, -- changes word jumps for CamelCase and underscore
 
-  {
-    'lukas-reineke/indent-blankline.nvim', -- indentation guides
-    main = "ibl",
-    opts = {},
-    config = function()
-      local highlight = {
-        "CursorColumn",
-        "Whitespace"
-      }
-      require('ibl').setup() 
-      -- {
-      --   indent = {highlight = highlight, char = "" },
-      --   whitespace = { highlight = highlight, remove_blankline_trail = false },
-      --   scope = { enabled = false }
-      -- }
-    end
-  },
+	{
+		"lukas-reineke/indent-blankline.nvim", -- indentation guides
+		main = "ibl",
+		opts = {},
+		config = function()
+			local highlight = {
+				"CursorColumn",
+				"Whitespace",
+			}
+			require("ibl").setup()
+			-- {
+			--   indent = {highlight = highlight, char = "" },
+			--   whitespace = { highlight = highlight, remove_blankline_trail = false },
+			--   scope = { enabled = false }
+			-- }
+		end,
+	},
 
-  { 'numToStr/Comment.nvim', -- comment toggling
-    config = function()
-      require('Comment').setup({
-        toggler = {
-          line = '<C-_>',
-        },
-        opleader = {
-          line = '<C-_>',
-        }
-      })
-    end
-  },
+	{
+		"numToStr/Comment.nvim", -- comment toggling
+		config = function()
+			require("Comment").setup({
+				toggler = {
+					line = "<C-_>",
+				},
+				opleader = {
+					line = "<C-_>",
+				},
+			})
+		end,
+	},
 
-  {
-    'nvim-lualine/lualine.nvim', -- Fancier statusline
-    dependencies = { 'kyazdani42/nvim-web-devicons' },
-    config = function()
-      require('lualine').setup {
-        options = {
-          icons_enabled = true,
-          theme = 'catppuccin',
-          component_separators = '|',
-          section_separators = '',
-        },
-      }
-    end
-  },
+	{
+		"nvim-lualine/lualine.nvim", -- Fancier statusline
+		dependencies = { "kyazdani42/nvim-web-devicons" },
+		config = function()
+			local noir = require("noirbuddy.plugins.lualine")
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					-- theme = 'catppuccin',
+					theme = noir.theme,
+					component_separators = "|",
+					section_separators = "",
+				},
+			})
+		end,
+	},
 
-  { 'kylechui/nvim-surround', -- Add/change/delete surrounding pairs
-    config = function() require('nvim-surround').setup() end
-  },
+	{
+		"kylechui/nvim-surround", -- Add/change/delete surrounding pairs
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	},
 
-  { 'karb94/neoscroll.nvim', -- smooth scroll for things like C-u, C-d
-    config = function()
-      require('neoscroll').setup({
-        easing_function = "sine"
-      })
-    end
-  },
+	{
+		"karb94/neoscroll.nvim", -- smooth scroll for things like C-u, C-d
+		config = function()
+			require("neoscroll").setup({
+				easing_function = "sine",
+			})
+		end,
+	},
 
-  { 'windwp/nvim-autopairs', -- auto closes brackets/qutoes
-    config = function()
-      require("nvim-autopairs").setup()
-    end
-  },
-
-  -- {
-  --   'zbirenbaum/copilot.lua',
-  --   config = function()
-  --     require('copilot').setup({
-  --       suggestion = { enabled = false },
-  --       panel = { enabled = false },
-  --       copilot_node_command = vim.g.system_node,
-  --     })
-  --   end
-  -- },
-
-  -- {
-  --   'zbirenbaum/copilot-cmp',
-  --   dependencies = { 'zbirenbaum/copilot.lua' },
-  --   config = function()
-  --     require("copilot_cmp").setup({})
-  --   end
-  -- },
-
-  -- {
-  --   'chipsenkbeil/distant.nvim', --easily edit remote files
-  --   cond = not vim.g.is_windows,
-  --   config = function()
-  --     require('distant').setup {
-  --       ['*'] = require('distant.settings').chip_default()
-  --     }
-  --   end
-  -- },
-
+	{
+		"windwp/nvim-autopairs", -- auto closes brackets/qutoes
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	},
 }
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,19 +1,78 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	config = function()
-		require("catppuccin").setup({
-			transparent_background = true,
-		})
-		vim.cmd([[colorscheme catppuccin]])
-	end,
+	{
+		"0xstepit/flow.nvim",
+		lazy = false,
+		priority = 1000,
+		tag = "v1.0.0",
+		opts = {
+			theme = {
+				style = "dark", --  "dark" | "light"
+				contrast = "default", -- "default" | "high"
+				transparent = false, -- true | false
+			},
+			colors = {
+				mode = "default", -- "default" | "dark" | "light"
+				fluo = "pink", -- "pink" | "cyan" | "yellow" | "orange" | "green"
+			},
+			ui = {
+				borders = "inverse", -- "theme" | "inverse" | "fluo" | "none"
+				aggressive_spell = false, -- true | false
+			},
+		},
+		-- config = function(_, opts)
+		-- 	require("flow").setup(opts)
+		-- 	vim.cmd("colorscheme flow")
+		-- end,
+	},
+
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		-- config = function()
+		-- 	vim.cmd("colorscheme rose-pine")
+		-- end,
+	},
+
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+			})
+			-- vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
+
+	{
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		-- config = function()
+		-- 	vim.cmd("colorscheme github_dark_default")
+		-- end,
+	},
+
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+
+	{
+		"jesseleite/nvim-noirbuddy",
+		dependencies = {
+			{ "tjdevries/colorbuddy.nvim" },
+		},
+		lazy = false,
+		priority = 1000,
+		opts = {
+			-- All of your `setup(opts)` will go here
+		},
+		config = function(_, opts)
+			require("noirbuddy").setup()
+			vim.cmd("colorscheme noirbuddy")
+		end,
+	},
 }
--- return {
--- 	"projekt0n/github-nvim-theme",
--- 	name = "github-theme",
--- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
--- 	priority = 1000, -- make sure to load this before all the other start plugins
--- 	config = function()
--- 		vim.cmd("colorscheme github_dark_default")
--- 	end,
--- }
